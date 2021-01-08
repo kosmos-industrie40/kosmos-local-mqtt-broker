a84057f101a70b3bf21a7f739ff51310482d2595
Andreas Clausnizer <clausnizer@ondics.de>
2021-01-08 09:27:24 +0100

    Push upstream Node-Red Version Cleanup Dockerfile, entrypoint

--- mqtt-dashboard/entrypoint.sh
+++ mqtt-dashboard/entrypoint.sh
@@ -4,27 +4,15 @@
 # Benötigt Environment Variablen
 # Für den Broker an sich:
 # * USE_TLS             - Soll der Broker mit SSL/TLS Starten?
-# * USE_PLAIN_AUTH      - Soll der Broker verbindungen ohne SSL/TLS erlauben? (Nicht implemtiert)
-#   * AUTH_USERNAME         - Benutzername (Nicht implemtiert)
-#   * AUTH_PASSWORD         - Passwort (Nicht implemtiert)
 #
-# Für die PKI
+# Für die PKI (Bei TLS)
 # * MY_PKI_URI          - Ist die vollständige URI der Vault PKI
 # * VAULT_TOKEN         - Ist der Vault-Token um Zertifikate zu erhalten
-# * MQTT_BROKER_FQDN    - Ist der vollständige MQTT-Broker-Domainname
 #
 if [ "$USE_TLS" = true ]; then
     # Der CN für das Zertifikat wird aus dem Domainname und dem Hostname erzeugt.
     export MY_FQDN=`hostname -f`
     . /app/common/request_cert.sh
-else
-    if [ "$USE_PLAIN_AUTH" = true ] && [ ! -z $AUTH_USERNAME ] && [ ! -z $AUTH_PASSWORD ]; then
-        #TODO?
-        echo -
-    else
-        #TODO?
-        echo -
-    fi
 fi
 
-npm start -- --userDir /app --settings /app/settings.js
+npm start -- --userDir /app
